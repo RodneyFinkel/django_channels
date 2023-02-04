8e82faeb207254e5d3619323cd27cedbfda796dc
AlexAleph0 <rfinkel@deloitte.co.il>
2023-02-04 19:57:52 +0200

    synch it

--- chat/static/room.js
+++ chat/static/room.js
@@ -1,14 +1,15 @@
-console.log('Sanity check from room.js')
+// chat/static/room.js
 
-const roomName = JSON.parse(document.getElementById('roomname').textContent);
+console.log("Sanity check from room.js.");
 
-let chatLog = document.querySelector('#chatLog');
-let chatMessageInput = document.querySelector('#chatMessageInput');
-let chatMessageSend = document.querySelector('#chatMessageSend');
-let onlineUsersSelector = document.querySelector('#onlineUserSelector');
+const roomName = JSON.parse(document.getElementById('roomName').textContent);
+
+let chatLog = document.querySelector("#chatLog");
+let chatMessageInput = document.querySelector("#chatMessageInput");
+let chatMessageSend = document.querySelector("#chatMessageSend");
+let onlineUsersSelector = document.querySelector("#onlineUsersSelector");
 
 // adds a new option to 'onlineUsersSelector'
-
 function onlineUsersSelectorAdd(value) {
     if (document.querySelector("option[value='" + value + "']")) return;
     let newOption = document.createElement("option");
@@ -18,27 +19,24 @@ function onlineUsersSelectorAdd(value) {
 }
 
 // removes an option from 'onlineUsersSelector'
-
 function onlineUsersSelectorRemove(value) {
     let oldOption = document.querySelector("option[value='" + value + "']");
-    if (oldOption !==null) oldOption.remove();
+    if (oldOption !== null) oldOption.remove();
 }
 
-//focus chatMessageInput when user opens the page
+// focus 'chatMessageInput' when user opens the page
+chatMessageInput.focus();
+
+// submit if the user presses the enter key
 chatMessageInput.onkeyup = function(e) {
-    if (e.keyCode === 13) {
+    if (e.keyCode === 13) {  // enter key
         chatMessageSend.click();
     }
 };
 
 // clear the 'chatMessageInput' and forward the message
-
 chatMessageSend.onclick = function() {
     if (chatMessageInput.value.length === 0) return;
-    //TODO forward the message to the Websocket
+    // TODO: forward the message to the WebSocket
     chatMessageInput.value = "";
 };
-
-
-
-
