19fdcc46242de3e8e6d990106573b82acdb79db4
Rodney Finkel <111357994+RodneyDeloitte@users.noreply.github.com>
2023-05-01 04:31:20 +0300

    synch it

--- chat/consumers.py
+++ chat/consumers.py
@@ -13,5 +13,37 @@ class ChatConsumer(WebsocketConsumer):
         
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'chat_{self.room_name}'
+        self.room = Room.object.get(name=self.room_name)
         
+        # connection has to be accepted
+        self.accept()
+        
+        # join the room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name,
+        )
+        
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name,
+        )
+        
+    def receive(self, text_data=None, bytes_data=None):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        
+        # send chat message event to room
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+            }
+        )
+        
+    def chat_message(self, event):
+        self.send(text_data=json.dumps(event))
     
